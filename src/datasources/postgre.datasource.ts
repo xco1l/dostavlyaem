e14606338430dffe5be8206dc05df003bd1a95d6
Saniyook <saniyook@gmail.com>
2019-12-05 12:30:53 +0300

    initalial app

---
+++ src/datasources/postgre.datasource.ts
@@ -0,0 +1,36 @@
+import {
+  inject,
+  lifeCycleObserver,
+  LifeCycleObserver,
+  ValueOrPromise,
+} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import * as config from './postgre.datasource.config.json';
+
+@lifeCycleObserver('datasource')
+export class PostgreDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'Postgre';
+
+  constructor(
+    @inject('datasources.config.Postgre', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+
+  /**
+   * Start the datasource when application is started
+   */
+  start(): ValueOrPromise<void> {
+    // Add your logic here to be invoked when the application is started
+  }
+
+  /**
+   * Disconnect the datasource when application is stopped. This allows the
+   * application to be shut down gracefully.
+   */
+  stop(): ValueOrPromise<void> {
+    return super.disconnect();
+  }
+}
