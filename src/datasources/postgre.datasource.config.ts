8d6a587352f841f86076e041b0f365024b5f7469
Saniyook <saniyook@gmail.com>
2019-12-05 18:02:09 +0300

    url property removed

--- src/datasources/postgre.datasource.config.ts
+++ src/datasources/postgre.datasource.config.ts
@@ -1,9 +1,6 @@
 const config = {
   name: 'Postgre',
   connector: 'postgresql',
-  url: `postgres://${process.env.DB_USER || 'postgres'}:${process.env.DB_PASS ||
-    'postgres'}@${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT ||
-    5432}/${process.env.DB_NAME || 'postgres'}`,
   host: `${process.env.DB_HOST}` || 'localhost',
   port: `${process.env.DB_PORT}` || 5432,
   user: `${process.env.DB_USER}` || 'postgres',
