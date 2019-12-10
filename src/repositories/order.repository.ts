e79c03373423cf0ddeb7d3fef3f7419a92a9c5b3
Saniyook <saniyook@gmail.com>
2019-12-10 13:14:46 +0300

    bounding user and order repos

--- src/repositories/order.repository.ts
+++ src/repositories/order.repository.ts
@@ -1,16 +1,26 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Order, OrderRelations} from '../models';
+import {
+  DefaultCrudRepository,
+  BelongsToAccessor,
+  repository,
+} from '@loopback/repository';
+import {Order, OrderRelations, User} from '../models';
 import {PostgreDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
+import {UserRepository} from './user.repository';
 
 export class OrderRepository extends DefaultCrudRepository<
   Order,
   typeof Order.prototype.id,
   OrderRelations
 > {
+  public readonly user: BelongsToAccessor<User, typeof Order.prototype.id>;
   constructor(
-    @inject('datasources.postgre') dataSource: PostgreDataSource,
+    @inject('datasources.postgre')
+    dataSource: PostgreDataSource,
+    @repository.getter('UserRepository')
+    userRepositoryGetter: Getter<UserRepository>,
   ) {
     super(Order, dataSource);
+    this.user = this.createBelongsToAccessorFor('user', userRepositoryGetter);
   }
 }
