31913bc60a0844bac6a599938dc35791d66c4faa
Saniyook <saniyook@gmail.com>
2019-12-13 15:36:40 +0300

    added react-router-dom and built app backbone

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+import {Home} from 'pages';
+import {Auth} from 'pages';
 
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="wrap">
+      <Switch>
+        <Route exact path={'/'} render={() => <Home />} />
+        <Route exact path={['/register', '/login']} render={() => <Auth />} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
