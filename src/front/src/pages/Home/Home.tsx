747778f71ec971d13c4d009293165e124fcefd9c
Saniyook <saniyook@gmail.com>
2019-12-17 18:54:29 +0300

    added home page grid

--- src/front/src/pages/Home/Home.tsx
+++ src/front/src/pages/Home/Home.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import {Header, Footer, Container} from 'components';
+import {Navigation} from 'containers';
+import './Home.scss';
 const Home: React.FC = () => {
-  return <section className="home"></section>;
+  return (
+    <div className="page">
+      <div className="page__wrapper">
+        <Header />
+        <Navigation />
+        <div className="page__content">
+          <Container>MENU</Container>
+        </div>
+      </div>
+      <Footer className="page__footer" />
+    </div>
+  );
 };
 
 export default Home;
