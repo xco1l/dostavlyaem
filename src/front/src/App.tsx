c6de982ec58933aa68d421e2b3a2ab2977bfe3de
Saniyook <saniyook@gmail.com>
2019-12-18 13:08:24 +0300

    added mobx devtool

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
+import DevTools from 'mobx-react-devtools';
 
 import {Home} from 'pages';
 import {Auth} from 'pages';
@@ -7,6 +8,7 @@ import {Auth} from 'pages';
 const App: React.FC = () => {
   return (
     <div className="wrap">
+      {process.env.NODE_ENV === 'DEVELOPMENT' && <DevTools />}
       <Switch>
         <Route exact path={'/'} render={() => <Home />} />
         <Route exact path={['/register', '/login']} render={() => <Auth />} />
