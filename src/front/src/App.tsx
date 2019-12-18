897d878fadf63b1e7f44f345ce2205779ea88288
Saniyook <saniyook@gmail.com>
2019-12-18 14:09:45 +0300

    added nav links getting from DB

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Route, Switch} from 'react-router-dom';
+import {Provider} from 'mobx-react';
+import {observable} from 'mobx';
 import DevTools from 'mobx-react-devtools';
 
 import {Home} from 'pages';
 import {Auth} from 'pages';
+import {axios} from 'core';
+
+const stores = observable({
+  navLinks: [],
+});
 
 const App: React.FC = () => {
+  const getLinks = async () => {
+    return await axios.get('/navlinks');
+  };
+
+  useEffect(() => {
+    stores.navLinks = getLinks() as any;
+  });
+
   return (
     <div className="wrap">
-      {process.env.NODE_ENV === 'DEVELOPMENT' && <DevTools />}
-      <Switch>
-        <Route exact path={'/'} render={() => <Home />} />
-        <Route exact path={['/register', '/login']} render={() => <Auth />} />
-      </Switch>
+      <Provider {...stores}>
+        {process.env.NODE_ENV === 'DEVELOPMENT' && <DevTools />}
+        <Switch>
+          <Route exact path={'/'} render={() => <Home />} />
+          <Route exact path={['/register', '/login']} render={() => <Auth />} />
+        </Switch>
+      </Provider>
     </div>
   );
 };
--- src/front/src/containers/Navigation.tsx
+++ src/front/src/containers/Navigation.tsx
@@ -1,18 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {Navigation as NavigationComponent} from 'components';
 import {NavigationProps} from 'components/Navigation/Navigation';
-
-const Navigation: React.SFC<NavigationProps> = ({className}) => {
-  const [navLinkFromDB, setNavLinks] = useState([]);
-
-  async function getLinks() {
-    const links = await fetch('myDB.com/api');
-    setNavLinks(links as any);
-  }
-  useEffect(() => {
-    getLinks();
-  });
-  return <NavigationComponent className={className} navLinks={navLinkFromDB} />;
+import {observer, inject} from 'mobx-react';
+const Navigation: React.SFC<NavigationProps> = ({className, navLinks}) => {
+  return <NavigationComponent className={className} navLinks={navLinks} />;
 };
 
-export default Navigation;
+export default inject('navLinks')(observer(Navigation));
