2936a792da99bccf85146908f8bec4c1672165b5
Saniyook <saniyook@gmail.com>
2019-12-27 14:16:29 +0300

    added dynamic imports of controllers

--- packages/auth/src/core/Server.ts
+++ packages/auth/src/core/Server.ts
@@ -5,6 +5,12 @@ import express, {
   NextFunction,
   Router,
 } from 'express';
+import 'reflect-metadata';
+import fs from 'fs';
+import {promisify} from 'util';
+import path from 'path';
+
+const readdir = promisify(fs.readdir);
 
 type Controller = InstanceType<any>;
 type RouterLib = (options?: any) => any;
@@ -12,6 +18,14 @@ type RouterLib = (options?: any) => any;
 interface IRouterAndPath {
   basePath: string | null;
   router: Router | null;
+}
+
+export interface BootOptions {
+  controllers?: {
+    dirs: Array<string>;
+    extensions: Array<string>;
+    nested: boolean;
+  };
 }
 
 export class Server {
@@ -25,11 +39,55 @@ export class Server {
     return this._app;
   }
 
-  protected addControllers(
-    controllers: Controller | Controller[],
+  //Default options
+  protected bootOptions: BootOptions = {
+    controllers: {
+      dirs: ['controllers'],
+      extensions: ['ts'],
+      nested: false,
+    },
+  };
+
+  protected async boot(routerLib?: RouterLib) {
+    await this.addControllers(routerLib);
+  }
+
+  private async addControllers(routerLib?: RouterLib) {
+    const Controllers = await this.pickUpControllers();
+    this.setUpControllers(Controllers, routerLib);
+  }
+
+  //TODO Make nested dirs scan
+  private async pickUpControllers() {
+    const Controllers = [];
+    const extensions = this.bootOptions.controllers.extensions;
+    const dirs = this.bootOptions.controllers.dirs;
+    for (const dir of dirs) {
+      const scannedModules = await readdir(
+        path.resolve(`${__dirname}/../${dir}`),
+      );
+
+      for (let module of scannedModules) {
+        for (const ext of extensions) {
+          if (module.indexOf(ext)) {
+            module = await import(`${__dirname}/../${dir}/${module}`);
+            const controllerKeys = Object.getOwnPropertyNames(module).filter(
+              key => key.indexOf('Controller') >= 0,
+            );
+            for (const controller of controllerKeys) {
+              Controllers.push(module[controller]);
+            }
+          }
+        }
+      }
+    }
+    return Controllers;
+  }
+
+  private setUpControllers(
+    controllers: Controller[],
     routerLib?: RouterLib,
   ): void {
-    controllers = controllers instanceof Array ? controllers : [controllers];
     const routerLibrary = routerLib || Router;
     controllers.forEach((controller: Controller) => {
       if (controller) {
@@ -41,16 +99,13 @@ export class Server {
     });
   }
 
-  protected getRouter(
-    routerLibrary: RouterLib,
-    controller: Controller,
-  ): IRouterAndPath {
+  private getRouter(Router, controller: Controller): IRouterAndPath {
     const prototype = controller.prototype;
     const options = Reflect.getOwnMetadata('OPTIONS', prototype);
 
     let router: any;
-    if (options) router = routerLibrary(options);
-    else router = routerLibrary();
+    if (options) router = Router(options);
+    else router = Router();
 
     const basePath = Reflect.getOwnMetadata('BASE_PATH', prototype);
     if (!basePath) {
@@ -60,8 +115,8 @@ export class Server {
       };
     }
 
-    let members = Object.getOwnPropertyNames(controller);
-    members = members.concat(Object.getOwnPropertyNames(prototype));
+    let members = Object.getOwnPropertyNames(prototype);
+
     members.forEach(member => {
       const route = prototype[member];
       const routeProperties = Reflect.getOwnMetadata(member, prototype);
