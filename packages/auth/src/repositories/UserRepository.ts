f2083b7eb4a2cd1cca59256678b66a993743d647
Saniyook <saniyook@gmail.com>
2020-01-17 18:37:15 +0300

    added user repository

--- packages/auth/src/repositories/UserRepository.ts
+++ packages/auth/src/repositories/UserRepository.ts
@@ -1,1 +1,18 @@
-//TODO UserController
+import {EntityRepository, EntityManager, Repository} from 'typeorm';
+import {User} from '../entities/User';
+
+@EntityRepository(User)
+export class UserRepository {
+  constructor(private manager: EntityManager) {}
+  createAndSave(userCreditionals: User): Promise<User> {
+    const user = new User();
+    user.email = userCreditionals.email;
+    user.password = userCreditionals.password;
+    user.userName = userCreditionals.userName;
+    return this.manager.save(user);
+  }
+
+  findById(id: string) {
+    return this.manager.findOne(User, {id});
+  }
+}
