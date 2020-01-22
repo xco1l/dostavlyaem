084e7c113bc8014cddf2bae0b7b51465a72efdcd
Saniyook <saniyook@gmail.com>
2020-01-22 14:40:56 +0300

    removed db config added repo getter

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,31 +1,13 @@
-import {AuthService} from './App';
-import {UserController} from './controllers';
-import {UserRepository} from './repositories';
-import {ConnectionOptions, EntitySchema} from 'typeorm';
-
+import {AuthService} from 'App';
+import {UserController} from 'controllers';
+import {getUserRepo} from '@/db/getters/getUserRepo';
 export async function main() {
-  const dbConfig: ConnectionOptions = {
-    type: 'postgres',
-    name: process.env.TYPEORM_CONNECTION,
-    host: `${process.env.TYPEORM_HOST}`,
-    port: Number(`${process.env.TYPEORM_PORT}`),
-    username: `${process.env.TYPEORM_USERNAME}`,
-    password: `${process.env.TYPEORM_PASSWORD}`,
-    database: `${process.env.TYPEORM_DATABASE}`,
-    entities: Array(`${process.env.TYPEORM_ENTITIES}`),
-    migrations: Array(`${process.env.TYPEORM_MIGRATIONS}`),
-    cli: {
-      migrationsDir: `${process.env.TYPEORM_MIGRATIONS_DIR}`,
-    },
-  };
-
   const app = new AuthService();
 
-  await app.boot(dbConfig);
+  const UserRepository = await getUserRepo();
+  app.bind('repositories.UserRepository').to(UserRepository);
 
-  app.repository(UserRepository);
   app.addControllers([UserController]);
-
   await app.start();
 }
 
